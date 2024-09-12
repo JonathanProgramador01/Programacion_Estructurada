0cb55862cbe242342bca1f58224884efa0fadcb5
Jonathan Rosas Cedillo <157512868+JonathanProgramador01@users.noreply.github.com>
2024-09-11 20:43:33 -0600

    Update 4_Operadores_de_asignacion.py

---
+++ 4_Operadores_de_asignacion.py
@@ -0,0 +1,49 @@
+## IÑAKII 
+
+#NO PUSO NINGUN EJERCICIO 
+
+
+
+
+####PARTEEE DE MARCOOOO 
+#SU EJEMPLO
+
+
+# Definimos una variable inicial
+a = 10
+print(f"Valor inicial de a: {a}")
+# Operador de suma y asignación
+a += 5 # a = a + 5
+print(f"Después de a += 5: {a}")
+# Operador de resta y asignación
+a -= 3 # a = a - 3
+print(f"Después de a -= 3: {a}")
+# Operador de multiplicación y asignación
+a *= 2 # a = a * 2
+print(f"Después de a *= 2: {a}")
+# Operador de división y asignación
+a /= 4 # a = a / 4
+print(f"Después de a /= 4: {a}")
+# Operador de módulo y asignación
+a %= 3 # a = a % 3
+print(f"Después de a %= 3: {a}")
+# Operador de potencia y asignación
+a **= 3 # a = a ** 3
+print(f"Después de a **= 3: {a}")
+# Operador de OR bitwise y asignación
+a |= 2 # a = a | 2
+print(f"Después de a |= 2: {a}")
+# Operador de XOR bitwise y asignación
+a ^= 1 # a = a ^ 1
+print(f"Después de a ^= 1: {a}")
+# Operador de AND bitwise y asignación
+a &= 1 # a = a & 1
+print(f"Después de a &= 1: {a}")
+# Operador de desplazamiento a la izquierda y
+# asignación
+a <<= 2 # a = a << 2
+print(f"Después de a <<= 2: {a}")
+# Operador de desplazamiento a la derecha y
+# asignación
+a >>= 1 # a = a >> 1
+print(f"Después de a >>= 1: {a}")
