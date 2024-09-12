2184e2945dbd3ac37f96206a74a88de1e775d648
Jonathan Rosas Cedillo <157512868+JonathanProgramador01@users.noreply.github.com>
2024-09-11 20:43:08 -0600

    Update 3_Variables_Constantes.py

--- 3_Variables_Constantes.py
+++ 3_Variables_Constantes.py
@@ -1,4 +1,46 @@
+###EMANUELLL
+
 mi_variable = 10
 MI_CONSTANTE = 3.14159
 print("Valor de mi_variable:", mi_variable)
 print("Valor de MI_CONSTANTE:",MI_CONSTANTE)
+
+
+
+###ESTOS EJERCICIOS LOS DEJO EL PROFESORR
+###PROGRAMAAA DE LA HAMBURGUESSAAA 
+hamburgesa = 130
+refrsco = 30
+papas = 20
+DESCUENTO = 0.20
+
+
+total = hamburgesa+refrsco+papas
+if hamburgesa and refrsco and papas:
+    #Ago el 20 porciento   
+    descuento = total*0.20
+    print("Descuento:",descuento, "Total",total-descuento)
+else:
+    print("No tienes descuento:",total)
+
+
+
+
+####EJERCICIOOO DE NUESTRAS PALOMITAS
+#1 kilo de maiz puedo hacer 43 bolsa
+BOLSAS =43
+PRECIO_KILO = 30
+PRECIO_DE_BOLSA =10
+
+
+ 
+
+operacion = 10 *43
+
+
+porcentaje = (operacion/PRECIO_KILO)
+print("TOTAL: ",operacion)
+print("Inverti:",30)
+print("Mi ganancioa total es de:",operacion-PRECIO_KILO)
+print("Porcentaje de ganacia: ",100-porcentaje)
+
